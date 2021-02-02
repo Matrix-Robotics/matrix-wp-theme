84a93ebfbd14e6572e82647fb930468c725f753b
Kevin Cheng <kevinphy@gmail.com>
2021-02-02 17:53:15 +0800

    updated and testing full site rwd

--- themes/matrix-theme/functions.php
+++ themes/matrix-theme/functions.php
@@ -19,7 +19,7 @@ if ( ! function_exists( 'matrix_theme_support' ) ) :
 		add_theme_support( 'editor-styles' );
 
 		// Enqueue editor styles.
-		add_editor_style( 'style.css' );
+		add_editor_style( 'matrix-theme-styles/editor-style.css' );
 
 		// Add support for custom units.
 		add_theme_support( 'custom-units' );
@@ -33,9 +33,9 @@ endif;
 function matrix_theme_scripts() {
 
 	// Enqueue theme stylesheet.
-	wp_enqueue_style( 'matrix-theme-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'matrix-theme-style', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ) );
 
 	// Enqueue alignments stylesheet.
-	wp_enqueue_style( 'matrix-theme-alignments-style', get_template_directory_uri() . '/assets/alignments-front.css', array(), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'matrix-theme-alignments-style', get_template_directory_uri() . '/matrix-theme-styles/alignments-front.css', array(), wp_get_theme()->get( 'Version' ) );
 }
 add_action( 'wp_enqueue_scripts', 'matrix_theme_scripts' );
