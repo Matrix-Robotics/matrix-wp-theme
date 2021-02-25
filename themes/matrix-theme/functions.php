b770542c6c199809f60037b6033fb518b187e78e
Kevin Cheng <kevinphy@gmail.com>
2021-02-25 18:37:19 +0800

    fixed normalize css file source, added rwd to footer, modified css structure

--- themes/matrix-theme/functions.php
+++ themes/matrix-theme/functions.php
@@ -42,9 +42,6 @@ function matrix_theme_scripts() {
 
 	// Enqueue theme stylesheet.
 	wp_enqueue_style( 'matrix-theme-style', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ) );
-
-	// Enqueue alignments stylesheet.
-	wp_enqueue_style( 'matrix-theme-alignments-style', get_template_directory_uri() . '/matrix-theme-styles/alignments-front.css', array(), wp_get_theme()->get( 'Version' ) );
 }
 add_action( 'wp_enqueue_scripts', 'matrix_theme_scripts' );
 
