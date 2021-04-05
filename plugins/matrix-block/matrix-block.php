327ecd14f7560f94ca356e5350df670c06ef48c5
Kevin Cheng <kevinphy@gmail.com>
2021-04-06 03:39:26 +0800

    set dev env for building block. register new block name 'Matrix Block'  showing in 'Add Block' category

--- plugins/matrix-block/matrix-block.php
+++ plugins/matrix-block/matrix-block.php
@@ -12,52 +12,45 @@
 
 defined( 'ABSPATH' ) || exit;
 
-function matrix_block_register_block() {
+include 'test/index.php';
+
+include '01-banner/index.php';
+include '02-call-to-action/index.php';
+include '03-breadcrumb/index.php';
+include '04-list/index.php';
+include '05-table/index.php';
+include '06-card-fluid/index.php';
+include '07-product-fluid/index.php';
+include '08-text-box/index.php';
+include '09-horizental-card/index.php';
+include '10-media-and-text/index.php';
+
+include '11-button/index.php';
+include '12-social-icon/index.php';
+
+include '13-store-item-banner/index.php';
+include '14-store-item-nav/index.php';
+include '15-store-category/index.php';
+
+
+// Register new Matrix block category in Gutenberg editor
+function matrix_block_categories( $categories ) {
+    // Plugin’s block category title and slug.
+    $block_category = array( 'title' => esc_html__( 'Matrix Block', 'text-domain' ), 'slug' => 'matrix-block' );
+    $category_slugs = wp_list_pluck( $categories, 'slug' );
  
-    // automatically load dependencies and version
-    $asset_file = include( plugin_dir_path( __FILE__ ) . 'build/index.asset.php');
+    if ( ! in_array( $block_category['slug'], $category_slugs, true ) ) {
+        $categories = array_merge(
+            array(
+                array(
+                    'slug' => 'matrix-block',
+                    'title' => __( 'Matrix Block', 'matrix-block' )
+                ),
+            ),
+            $categories
+        );
+    }
  
-    wp_register_script(
-        'gutenberg-examples-01-esnext',
-        plugins_url( 'build/index.js', __FILE__ ),
-        $asset_file['dependencies'],
-        $asset_file['version']
-    );
- 
-    register_block_type( 'gutenberg-examples/example-01-basic-esnext', array(
-        'apiVersion' => 2,
-        'editor_script' => 'gutenberg-examples-01-esnext',
-    ) );
- 
+    return $categories;
 }
-add_action( 'init', 'matrix_block_register_block' );
-
-
-function matrix_gutenberg_enqueue() {
-    wp_enqueue_script(
-        'matrix-block-script',
-        plugins_url( 'matrix-block.js', __FILE__ )
-    );
-}
-add_action( 'enqueue_block_editor_assets', 'matrix_gutenberg_enqueue' );
-
-
-function sidebar_plugin_register() {
-    wp_register_script(
-        'plugin-sidebar-js',
-        plugins_url( 'plugin-sidebar.js', __FILE__ ),
-        array( 'wp-plugins', 'wp-edit-post', 'wp-element' )
-    );
-}
-add_action( 'init', 'sidebar_plugin_register' );
-
-
-function sidebar_plugin_script_enqueue() {
-    wp_enqueue_script( 'plugin-sidebar-js' );
-    register_post_meta( 'post', 'sidebar_plugin_meta_block_field', array(
-        'show_in_rest' => true,
-        'single' => true,
-        'type' => 'string',
-    ) );
-}
-add_action( 'enqueue_block_editor_assets', 'sidebar_plugin_script_enqueue' );
+add_filter( 'block_categories', 'matrix_block_categories' );
