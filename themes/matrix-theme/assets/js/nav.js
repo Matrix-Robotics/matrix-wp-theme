5a582cca9758e9758f352ae105c9dfea0aaa48b5
Kevin Cheng <kevinphy@gmail.com>
2021-05-17 09:19:24 +0800

    added fixed nav on top feature

--- themes/matrix-theme/assets/js/nav.js
+++ themes/matrix-theme/assets/js/nav.js
@@ -4,18 +4,31 @@
  * @package matrix theme
  */
 
-(function( $ ) {
+(function ($) {
 
-	masthead            = $( 'header' );
-	searchBarToggle     = masthead.find( '#search-btn' );
-    searchBarInput      = masthead.find( '#is-search-input-65' );
-    mobileMenuToggle    = masthead.find( '#menu-btn' );
-    mobileMenu          = masthead.find( '.nav-menu-mobile' );
-    i18nToggle          = masthead.find( '#i18n-btn' );
-    i18nIcon            = masthead.find( '.i18n-icon' );
-    cartLIcon           = masthead.find( '.cart-icon-desktop' );
-    
-    i18nToggle.change(function() {
+    masthead = $('header.wp-block-template-part');
+    searchBarToggle = masthead.find('#search-btn');
+    searchBarInput = masthead.find('#is-search-input-65');
+    mobileMenuToggle = masthead.find('#menu-btn');
+    mobileMenu = masthead.find('.nav-menu-mobile');
+    i18nToggle = masthead.find('#i18n-btn');
+    i18nIcon = masthead.find('.i18n-icon');
+    cartLIcon = masthead.find('.cart-icon-desktop');
+
+    prevScrollpos = window.pageYOffset;
+
+    window.onscroll = function () {
+        currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            masthead.css("top", "0" );
+        } else {
+            masthead.css("top", "-80px" );
+        }
+        prevScrollpos = currentScrollPos;
+        console.log(prevScrollpos);
+    };
+
+    i18nToggle.change(function () {
         if ($(this).is(':checked')) {
             i18nIcon.addClass('click-label');
         } else {
@@ -23,6 +36,6 @@
         }
     });
 
-   
 
-})( jQuery );
+
+})(jQuery);
