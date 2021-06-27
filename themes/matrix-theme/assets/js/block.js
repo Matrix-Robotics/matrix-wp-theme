24b529ca6f6e1f3a1173a44f1835cabbe84d44fe
Kevin Cheng <kevinphy@gmail.com>
2021-06-28 03:34:57 +0800

    updated card hover and click animation

--- themes/matrix-theme/assets/js/block.js
+++ themes/matrix-theme/assets/js/block.js
@@ -7,6 +7,8 @@
 (function ($) {
 
     listBlock           = $('.wp-block-matrix-list ul li');
+
+    cards               = $('.products .product.type-product, .wc-block-grid__product, .wp-block-matrix-text-box-download, .wp-block-matrix-text-box-lesson, .wp-block-matrix-text-box-started, .wp-block-matrix-text-box-support, .wp-block-matrix-horizental-card');
     
     listBlock.on("click", function (e) {
         if ($(this).parent().has("ul")) {
@@ -14,6 +16,10 @@
         }
         $(this).find('> ul').slideToggle(50);
         $(this).toggleClass( "bounce" );
+    });
+
+    cards.on("click", function (e) {
+        $(this).toggleClass( "selected" );
     });
 
     listBlock.each(function() {
